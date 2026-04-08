921fed23fe66d841945e24e13b4b26a91271a982
tewicklund <tewicklund@ucdavis.edu>
2026-04-08 16:44:02 -0700

    major functions all written, next comes testing with an acutal xitron power analyzer

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,5 @@
 from screeninfo import get_monitors
-import time
-import socket
+
 
 # function for sizing UI window (viewport) based on primary monitor width and height
 def compute_window_size(width=None, height=None):
@@ -38,4 +37,3 @@ def prep_float_for_disp(input_float_string,num_chars=8):
             output_float_length=len(output_float_string)
         return output_float_string
 
-def get_xitron_response(open_socket,)
--- python_xitron_dear_gui.py
+++ python_xitron_dear_gui.py
@@ -1,6 +1,12 @@
 import dearpygui.dearpygui as dpg
 from helper_functions import *
 from pathlib import Path
+import time
+import socket
+import threading
+
+# threading event to make stop button work
+stop_event=threading.Event()
 
 
 #-------------------------------------------------- CALLBACK SECTION --------------------------------------------------#
@@ -23,61 +29,94 @@ def decrement_page():
         dpg.set_value('PA_ID',PA_names[current_page_num])
 
 
-
-# start test callback function
 def start_test():
+    stop_event.clear()
+    threading.Thread(target=worker, daemon=True).start()
+
+
+# main test worker function
+def worker():
+    # set indicator bool test_running to True
+    global test_running
+    test_running=True
+
     # collect input parameters from GUI
     log_file_full_path=dpg.get_value("folder_path_text")+'/'+dpg.get_value("test_name_text")+'.csv'
     print(log_file_full_path)
     num_harmonics=dpg.get_value("num_harmonics_int")
     logging_period_seconds_float=float(dpg.get_value("log_period_ms_text"))/1000.0
+    if dpg.get_value("test_duration_text") == "":
+        test_duration_seconds_float=604800.0
+    else:
+        test_duration_seconds_float=float(dpg.get_value("test_duration_text"))
     extra_data_string=dpg.get_value("extra_data_text")
+    PA_IPs=[dpg.get_value('PA_IP1'),dpg.get_value('PA_IP2'),dpg.get_value('PA_IP3'),dpg.get_value('PA_IP4')]
+    PA_ports=[dpg.get_value('PA_port1'),dpg.get_value('PA_port2'),dpg.get_value('PA_port3'),dpg.get_value('PA_port4')]
+
+    # compute number of samples
+    num_samples=int(test_duration_seconds_float/logging_period_seconds_float)
+
+    # import query string that is sent to power analyzers
+    f=open("query_string.txt",'r')
+    query_string=f.readline().removesuffix('\n')
+    f.close()
+
+    # add harmonics and extra parameters to the query string
+    for channel_num in range(len(channel_names)):
+        for x in range(num_harmonics):
+            query_string+=f',V:CH{channel_num+1}:H{x+1},A:CH{channel_num+1}:H{x+1}'
+    query_string+=','+extra_data_string+'\n'
+    print(query_string)
+
+    # open sockets for power analyzers
+    for x in range(len(PA_names)):
+        PA_sockets=[]
+        if PA_IPs[x] != "" and PA_ports[x] != "":
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((PA_IPs[x],PA_ports[x]))
+            s.settimeout(10)
+            PA_sockets.append(s)
 
 
 
-    # f=open("sample_response.txt",'r')
-    # sample_response_string=f.readline()
-    # f.close()
-    # render_feedback(sample_response_string)
-    pass
+    # main test loop
+    with open(log_file_full_path,'w') as f:
+        # add column headers to first row
+        for socket_num in range(len(PA_sockets)):
+            column_headers_per_analyzer=query_string.removeprefix('READ?,').removesuffix('\n')
+            column_headers_per_analyzer.replace(',',f':PA{socket_num},')
+            f.write(column_headers_per_analyzer)
+        f.write('\n')
+
+        # log and display each sample till end of test
+        for sample_num in range(num_samples):
+            if stop_event.is_set():
+                print("Stopped!")
+                return
+            reading_time=time.time()
+            for socket_num in range(len(PA_sockets)):
+                PA_sockets[socket_num].sendall(query_string.encode())
+                response_string=PA_sockets[socket_num].recv(4096).decode()
+                current_page_num=PA_names.index(dpg.get_value('PA_ID'))
+                if current_page_num == socket_num:
+                    render_feedback(response_string)
+                f.write(response_string).removesuffix('\r\n')
+            f.write('\n')
+            print(f"Got sample {sample_num} of {num_samples}")
+            while(time.time()<reading_time+logging_period_seconds_float):
+                pass
+
+    # close sockets after test is done
+    for socket_num in range(len(PA_sockets)):
+        PA_sockets[socket_num].close()
+
 
 # stop test callback function
 def stop_test():
-    pass
+    stop_event.set()
 
 
 #-------------------------------------------------- CALLBACK SECTION --------------------------------------------------#
-
-
-# function for converting response string to display
-def render_feedback(response_string_raw):
-    response_list=response_string_raw.split(',')
-    response_index=0
-    for chan in channel_names:
-        dpg.set_value(f'V_RMS_{chan}',prep_float_for_disp(response_list[response_index]))
-        response_index+=1
-        dpg.set_value(f'V_AC_{chan}',prep_float_for_disp(response_list[response_index]))
-        response_index+=1
-        dpg.set_value(f'V_DC_{chan}',prep_float_for_disp(response_list[response_index]))
-        response_index+=1
-        dpg.set_value(f'A_RMS_{chan}',prep_float_for_disp(response_list[response_index]))
-        response_index+=1
-        dpg.set_value(f'A_AC_{chan}',prep_float_for_disp(response_list[response_index]))
-        response_index+=1
-        dpg.set_value(f'A_DC_{chan}',prep_float_for_disp(response_list[response_index]))
-        response_index+=1
-        dpg.set_value(f'W_RMS_{chan}',prep_float_for_disp(response_list[response_index]))
-        response_index+=1
-        dpg.set_value(f'W_AC_{chan}',prep_float_for_disp(response_list[response_index]))
-        response_index+=1
-        dpg.set_value(f'W_DC_{chan}',prep_float_for_disp(response_list[response_index]))
-        response_index+=1
-        dpg.set_value(f'PF_{chan}',prep_float_for_disp(response_list[response_index]))
-        response_index+=1
-
-
-
-
 
 
 # dearpygui setup
@@ -145,45 +184,42 @@ with dpg.window( pos=(0,0),width=ctrl_width,height=ctrl_height,no_move=True,no_r
             dpg.bind_item_font(full_path_text,tiny_font)
 
 
-        
+        # Number of harmonics to log
         with dpg.table_row():
             dpg.add_text("Num Harmonics to Log:")
-            dpg.add_input_int(tag='num_harmonics_int')
+            dpg.add_input_int(tag='num_harmonics_int',default_value=13)
 
-        
+        # time between readings
         with dpg.table_row():
             dpg.add_text("Logging Period (ms):")
-            dpg.add_input_text(width=-1,hint="Enter integer between 100 and 10,000",tag="log_period_ms_text")
+            dpg.add_input_text(width=-1,hint="Enter integer between 100 and 10,000",tag="log_period_ms_text",default_value="1000")
 
         
-
+        # total test duration, leaving blank will set the test time to 1 week
         with dpg.table_row():
             dpg.add_text("Test Duration (s)")
-            dpg.add_input_text(width=-1,hint="Leave 0 for indefinite logging")
+            dpg.add_input_text(width=-1,hint="Leave blank for indefinite logging",tag="test_duration_text")
 
         
-
+        # put other desired parameters to log here
         with dpg.table_row():
             dpg.add_text("Extra Data to Log")
             dpg.add_input_text(width=-1,hint="ex. V:CH1:CF,A:CH1:CF,V:CH2:CF,A:CH2:CF",tag="extra_data_text")
 
        
-
-
         with dpg.table_row():
             dpg.add_text(" ")
             dpg.add_text(" ")
 
         # Xitron network details
-        for x in range(4):
-            print(f"creating xitron ip section {x+1}")
+        for x in range(len(PA_names)):
             with dpg.table_row():
                 dpg.add_text(f"Xitron {x+1} IP Address:")
-                dpg.add_input_text(width=-1)
+                dpg.add_input_text(width=-1,tag=f'PA_IP{x+1}')
 
             with dpg.table_row():
                 dpg.add_text(f"Xitron {x+1} Port:")
-                dpg.add_input_text(width=-1)
+                dpg.add_input_text(width=-1,tag=f'PA_port{x+1}')
 
             with dpg.table_row():
                 dpg.add_text(" ")
@@ -207,6 +243,34 @@ with dpg.window( pos=(0,ctrl_height),width=ctrl_width,height=button_window_heigh
 
 
 #------------------------------------------------- FEEDBACK SECTION --------------------------------------------------#
+
+# function for converting response string to display
+def render_feedback(response_string_raw):
+    response_list=response_string_raw.split(',')
+    response_index=0
+    for chan in channel_names:
+        dpg.set_value(f'V_RMS_{chan}',prep_float_for_disp(response_list[response_index]))
+        response_index+=1
+        dpg.set_value(f'V_AC_{chan}',prep_float_for_disp(response_list[response_index]))
+        response_index+=1
+        dpg.set_value(f'V_DC_{chan}',prep_float_for_disp(response_list[response_index]))
+        response_index+=1
+        dpg.set_value(f'A_RMS_{chan}',prep_float_for_disp(response_list[response_index]))
+        response_index+=1
+        dpg.set_value(f'A_AC_{chan}',prep_float_for_disp(response_list[response_index]))
+        response_index+=1
+        dpg.set_value(f'A_DC_{chan}',prep_float_for_disp(response_list[response_index]))
+        response_index+=1
+        dpg.set_value(f'W_RMS_{chan}',prep_float_for_disp(response_list[response_index]))
+        response_index+=1
+        dpg.set_value(f'W_AC_{chan}',prep_float_for_disp(response_list[response_index]))
+        response_index+=1
+        dpg.set_value(f'W_DC_{chan}',prep_float_for_disp(response_list[response_index]))
+        response_index+=1
+        dpg.set_value(f'PF_{chan}',prep_float_for_disp(response_list[response_index]))
+        response_index+=1
+        dpg.set_value(f'FREQ_{chan}',prep_float_for_disp(response_list[response_index]))
+        response_index+=1
 
 with dpg.window( pos=(ctrl_width,0),width=fb_width,height=fb_height,no_move=True,no_resize=True,no_title_bar=True):
 
@@ -310,6 +374,13 @@ with dpg.window( pos=(ctrl_width,0),width=fb_width,height=fb_height,no_move=True
             dpg.add_text("POWER FACTOR")
             for chan in channel_names:
                 tag_string=f"PF_{chan}"
+                dpg.add_text("0.000000",tag=tag_string)
+                dpg.bind_item_font(tag_string,feedback_font)
+
+        with dpg.table_row():
+            dpg.add_text("FREQUENCY")
+            for chan in channel_names:
+                tag_string=f"FREQ_{chan}"
                 dpg.add_text("0.000000",tag=tag_string)
                 dpg.bind_item_font(tag_string,feedback_font)
 
